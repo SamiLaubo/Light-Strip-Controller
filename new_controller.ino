0c1cd2bf98f967e1051ccdab17631161e9896d3f
Sami Laubo <slaubo99@gmail.com>
2022-03-28 00:38:33 +0200

    Update new_controller.ino

--- new_controller.ino
+++ new_controller.ino
@@ -1,3 +1,8 @@
+// STD
+#include <string>
+#include <vector>
+#include <memory>
+
 // LED
 #include <FastLED.h>
 #define LED_PIN     5
@@ -28,7 +33,13 @@ public:
 };
 
 BUTTON::BUTTON(int pin) 
-    : pin { pin }, curr_state { HIGH }, prev_state { HIGH }, ret_val { false } {}
+    :   pin { pin }, 
+        curr_state { HIGH }, 
+        prev_state { HIGH }, 
+        ret_val { false }
+{
+    pinMode(pin, INPUT_PULLUP);
+}
 
 bool BUTTON::pressed()
 {
@@ -58,7 +69,10 @@ public:
 };
 
 POTENTIOMETER::POTENTIOMETER(int pin)
-    : pin { pin }, curr_val { 0 }, prev_val { 0 }, max { 1023 } {}
+    :   pin { pin }, 
+        curr_val { 0 }, 
+        prev_val { 0 }, 
+        max { 1023 } {}
 
 bool POTENTIOMETER::update()
 {
@@ -74,35 +88,136 @@ int POTENTIOMETER::value() const
 }
 
 
-class MODE
+class BASE_MODE
 {
 public:
-    MODE();
+    BASE_MODE(std::string name);
+    virtual void update(CRGB& leds, int saturation, int brightness) = 0;
+    std::string get_name() const;
 
 protected:
-    virtual void update() = 0;
+    const std::string name;
 };
 
-class RAINBOW : public MODE
+BASE_MODE::BASE_MODE(std::string name)
+    :   name { name } {} 
+
+std::string BASE_MODE::get_name() const 
 {
+    return name;
+}
 
+
+// -- RED light strip --
+class RED : public BASE_MODE
+{
+public:
+    RED(std::string name);
+    void update(CRGB& leds, int saturation, int brightness) override;
 };
+
+RED::RED(std::string name)
+    :   BASE_MODE(name) {}
+
+void RED::update(CRGB& leds, int saturation, int brightness)
+{
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CHSV(0, saturation, brightness);  // Hue = 0 -> Red
+    }
+}
+
+
+// -- RAINBOW light strip --
+class RAINBOW : public BASE_MODE
+{
+public:
+    RAINBOW(std::string name);
+    void update(CRGB& leds, int saturation, int brightness) override;
+};
+
+RAINBOW::RAINBOW(std::string name)
+    :   BASE_MODE(name) {}
+
+void RAINBOW::update(CRGB& leds, int saturation, int brightness)
+{
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CHSV(100, saturation, brightness);  // Hue = 0 -> Red
+    }
+}
+
+
+// -- MODE --
+class MODE
+{
+private:
+    int mode;
+    std::vector<std::unique_ptr<BASE_MODE>> modes;
+
+public:
+    MODE();
+    void operator++();
+    void update(CRGB& leds, int saturation, int brightness);
+    std::string get_name() const;
+};
+
+MODE::MODE()
+    :   mode { 0 },
+        modes { std::vector<std::unique_ptr<BASE_MODE>>(2) }
+{
+    modes[0] = std::make_unique<RED>("RED");
+    modes[1] = std::make_unique<RAINBOW>("Rainbow");
+}
+
+void MODE::operator++()
+{
+    mode = (mode + 1) % modes.size();
+}
+
+void MODE::update(CRGB& leds, int saturation, int brightness)
+{
+    modes[mode] -> update(leds, saturation, brightness);
+}
+
+std::string MODE::get_name() const
+{
+    return modes[mode] -> get_name();
+}
+
 
 // -- CONTROLLER --
 class CONTROLLER
 {
 private:
+    // Hardware
     BUTTON button;
     POTENTIOMETER pot_brightness;
     POTENTIOMETER pot_hue;
+
+    // LED strip
     CRGB leds[NUM_LEDS];
+    CRGB temp_leds[NUM_LEDS];
+
+    // Mode
+    enum class mode;
+    mode operator++(mode& m);
+
+    // Linear interpolation
+    bool do_lerp;
+    const int lerp_length;
+    int cur_lerp;
+
+    void change_mode();
 
 public:
     CONTROLLER();
 
     void update();
-    void change_mode();
 };
+
+void CONTROLLER::update()
+{
+
+}
 
 void setup()
 {
